aac1c20c79a58c13297ee8ddab309ae5d89ced4e
luowenlin <403305801@qq.com>
2018-03-26 14:07:43 +0800

    [code]  遗漏bean

---
+++ app/src/main/java/com/third/ytbus/bean/ADPlayBean.java
@@ -0,0 +1,42 @@
+package com.third.ytbus.bean;
+
+/**
+ * 作者：Sky on 2018/3/19.
+ * 用途：//TODO
+ */
+
+public class ADPlayBean {
+
+    //广告时间
+    private String adPlayStartTime;
+
+    //广告视频地址
+    private String adPlayPath;
+
+    //广告内容
+    private String adContent;
+
+    public String getAdPlayStartTime() {
+        return adPlayStartTime;
+    }
+
+    public void setAdPlayStartTime(String adPlayStartTime) {
+        this.adPlayStartTime = adPlayStartTime;
+    }
+
+    public String getAdPlayPath() {
+        return adPlayPath;
+    }
+
+    public void setAdPlayPath(String adPlayPath) {
+        this.adPlayPath = adPlayPath;
+    }
+
+    public String getAdContent() {
+        return adContent;
+    }
+
+    public void setAdContent(String adContent) {
+        this.adContent = adContent;
+    }
+}
